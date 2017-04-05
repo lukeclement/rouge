954ea44b1f59af0f937193b599b45e128d06751f
lukeclement <lclement.57468@farnborough.ac.uk>
2017-04-05 13:28:07 +0100

    Stillsome

--- main.java
+++ main.java
@@ -11,7 +11,7 @@ import javafx.animation.*;
 import javafx.event.*;
 
 public class main extends Application{
-    public static int HEIGHT=1024;
+    public static int HEIGHT=640;
     public static int WIDTH=1024;
 
     public static void main(String[] args){
@@ -63,10 +63,11 @@ public class main extends Application{
         List<Frog> frogs=new ArrayList<>();
         frogs.add(new Frog(20,20));
         System.out.println("Making maze");
-        final Maze mazy=new Maze(WIDTH,HEIGHT);
+        final Maze mazy=new Maze(1024,1024);
         System.out.println("Made maze!");
         Image back=new Image("res/Background/back.png");
-
+        Image floor = new Image("res/Background/fancywall.png");
+        Image white = new Image("res/Background/white.png");
 
         new AnimationTimer(){
             public boolean generated=false;
@@ -78,9 +79,9 @@ public class main extends Application{
             int smallOffsetY = 0;
             int offsetX = WIDTH/2;
             int offsetY = HEIGHT/2;
+            int[][] map = mazy.getGrid();
 
             public void drawBacking(){
-                int[][] map = mazy.getGrid();
                 if(smallOffsetX>64){
                     smallOffsetX=0;
                     offsetX++;
@@ -98,8 +99,6 @@ public class main extends Application{
                 for(int i=-1;i<(WIDTH/64)+1;i++){
                     for(int j=-1;j<(HEIGHT/64)+1;j++){
                         //Draw new images
-                        Image floor = new Image("res/Background/fancywall.png");
-                        Image white = new Image("res/Background/white.png");
                         if(map[(offsetX+i)][offsetY+j]==1){
                             gc.drawImage(floor,i*64-smallOffsetX,j*64-smallOffsetY);
 
@@ -117,12 +116,12 @@ public class main extends Application{
 
             //Updating the scene at 60 times per second
             public void handle(long currentNanoTime){
-                gc.drawImage(back,0,0);
+                //gc.drawImage(back,0,0);
                 double t = (currentNanoTime - startNanoTime) / 1000000000.0;
-                if(t>0.1){
-                    startNanoTime=System.nanoTime();
+                //if(t>0.1){
+                  //  startNanoTime=System.nanoTime();
                     //mazy.generate(s++);
-                }
+                //}
                 /*if(input.contains("A")){
                     gc.drawImage(back,0,0);
                     mazy.generate(0);
